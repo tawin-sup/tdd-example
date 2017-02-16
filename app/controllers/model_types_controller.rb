0ffae173fe22f4833c95c5d7b1b67e24705a29e7
game <tawin.sup@gmail.com>
2017-02-16 23:37:21 +0700

    Implement model types request

--- app/controllers/model_types_controller.rb
+++ app/controllers/model_types_controller.rb
@@ -2,7 +2,7 @@
 class ModelTypesController < ApplicationController
   def index
     @model = Model.find_by!(model_slug: params[:model_slug])
-    render json: {models: [serialized_model]}
+    render json: { models: [serialized_model] }
   end
 
   def show
