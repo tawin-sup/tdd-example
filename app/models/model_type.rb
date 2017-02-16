abd1bddc3fc7c09fe0b02e5426994aa7ca7f67d2
game <tawin.sup@gmail.com>
2017-02-16 22:05:41 +0700

    Implement total price

--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -4,4 +4,15 @@ class ModelType < ActiveRecord::Base
 
   validates_presence_of :name, :model_type_slug, :model_type_code, :base_price
   validates_uniqueness_of :name, :model_type_code, :model_type_slug
+
+  def total_price
+    case model.organization.pricing_policy
+    when 'flexible'
+      Pricing::Flexible.new(base_price).total_price
+    when 'fixed'
+      Pricing::Fixed.new(base_price).total_price
+    when 'prestige'
+      Pricing::Prestige.new(base_price).total_price
+    end
+  end
 end
