789cf9ad2544750455ebc837b835e28aa6c8cf33
game <tawin.sup@gmail.com>
2017-02-16 21:05:25 +0700

    Add spec for flexible price

--- spec/services/pricing/flexible_spec.rb
+++ spec/services/pricing/flexible_spec.rb
@@ -15,13 +15,71 @@ RSpec.describe Pricing::Flexible, type: :service do
     it 'opens reuters url' do
       VCR.use_cassette('reuters') do
         allow(subject).to receive(:open).
-            with('http://www.reuters.com')
+          with('http://www.reuters.com')
         subject.margin
         expect(subject).to have_received(:open).
-            with('http://www.reuters.com')
+          with('http://www.reuters.com')
       end
     end
 
+    context 'not count a in tag' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body><a> test </a></body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('http://www.reuters.com').
+          and_return(fake_html)
+        expect(subject.margin).to equal(0.0)
+      end
+    end
 
+    context 'Count a charactor' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body>aaabbbccc</body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('http://www.reuters.com').
+          and_return(fake_html)
+        expect(subject.margin).to equal(3/100.00)
+      end
+    end
+
+    context 'Not count a in style' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body><style>aaaa</style></body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('http://www.reuters.com').
+          and_return(fake_html)
+        expect(subject.margin).to equal(0.0)
+      end
+    end
+
+    context 'Not count a in script' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body><script>aaaa</script></body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('http://www.reuters.com').
+          and_return(fake_html)
+        expect(subject.margin).to equal(0.0)
+      end
+    end
   end
 end
