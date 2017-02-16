4d876f1b8f23fd1d6c47d6b37509560b98478640
game <tawin.sup@gmail.com>
2017-02-16 23:45:12 +0700

    Fix duplicate slug

--- spec/factories/model_types.rb
+++ spec/factories/model_types.rb
@@ -2,8 +2,8 @@
 FactoryGirl.define do
   factory :model_type do
     name { Faker::Name.name }
-    model_type_slug { Faker::Internet.slug }
-    model_type_code { Faker::Number.between(1, 99).to_s }
+    model_type_slug { Faker::Address.building_number }
+    model_type_code { Faker::Number.between(1, 100000).to_s }
     base_price { Faker::Number.normal(50, 3.5) }
   end
 end
