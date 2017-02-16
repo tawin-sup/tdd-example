3651eb6a2856f55b9388fb5eca63cf15aa8ac010
game <tawin.sup@gmail.com>
2017-02-17 00:06:59 +0700

    Implement model type serializer

--- app/controllers/model_types_controller.rb
+++ app/controllers/model_types_controller.rb
@@ -6,10 +6,15 @@ class ModelTypesController < ApplicationController
   end
 
   def show
-    render json: []
+    @model_type = Model.find_by!(model_slug: params[:model_slug]).model_types.find_by!(model_type_slug: params[:model_type_slug])
+    render json: { model_type: serialized_model_type }
   end
 
   private def serialized_model
     ModelSerializer.new(@model).as_json
+  end
+
+  private def serialized_model_type
+    ModelTypeSerializer.new(@model_type).as_json
   end
 end
