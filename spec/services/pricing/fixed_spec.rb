4cf91e43684ba6aae537baecc47be4284c2085cb
game <tawin.sup@gmail.com>
2017-02-16 20:39:04 +0700

    Add specs for count status

--- spec/services/pricing/fixed_spec.rb
+++ spec/services/pricing/fixed_spec.rb
@@ -21,5 +21,87 @@ RSpec.describe Pricing::Fixed, type: :service do
           with('https://developer.github.com/v3/#http-redirects')
       end
     end
+
+    it 'calls parse response' do
+      VCR.use_cassette('github') do
+        allow(subject).to receive(:parse_body).and_return('Status status')
+        subject.margin
+        expect(subject).to have_received(:parse_body)
+      end
+    end
+
+    it 'calls count status response' do
+      VCR.use_cassette('github') do
+        allow(subject).to receive(:count_status).and_return(14)
+        subject.margin
+        expect(subject).to have_received(:count_status)
+      end
+    end
+
+    context 'found status' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body>Status status word html test</body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('https://developer.github.com/v3/#http-redirects').
+          and_return(fake_html)
+        expect(subject.margin).to equal(2)
+      end
+    end
+
+    context 'not found status' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body>word html test</body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('https://developer.github.com/v3/#http-redirects').
+          and_return(fake_html)
+        expect(subject.margin).to equal(0)
+      end
+    end
+
+    context 'not count status in script tag' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body>
+              word html test
+              <script> status </script>
+          </body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('https://developer.github.com/v3/#http-redirects').
+          and_return(fake_html)
+        expect(subject.margin).to equal(0)
+      end
+    end
+
+    context 'not count status in style tag' do
+      fake_html = <<-EOS
+        <html>
+          <head><title>Some Title</title></head>
+          <body>
+              <style> status </style>
+              word html test
+          </body>
+        </html>
+      EOS
+      it do
+        allow(subject).to receive(:open).
+          with('https://developer.github.com/v3/#http-redirects').
+          and_return(fake_html)
+        expect(subject.margin).to equal(0)
+      end
+    end
   end
 end
