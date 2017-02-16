dae6916ea961f4152923a28db01aa2591f3b7c15
game <tawin.sup@gmail.com>
2017-02-16 19:49:44 +0700

    Add validation and define enum spec for organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Define Enum
+  it { is_expected.to define_enum_for(:organization_type).with([:show_room, :service, :dealer]) }
+  it { is_expected.to define_enum_for(:pricing_policy).with([:flexible, :fixed, :prestige]) }
+
+  # Validation
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:public_name) }
+  it { is_expected.to validate_presence_of(:organization_type) }
+  it { is_expected.to validate_presence_of(:pricing_policy) }
+
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:public_name) }
 end
