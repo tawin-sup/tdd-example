88b4bf94955fc2179bac11c114c3403990f9921c
game <tawin.sup@gmail.com>
2017-02-16 21:22:53 +0700

    Add prestige spec

--- spec/services/pricing/prestige_spec.rb
+++ spec/services/pricing/prestige_spec.rb
@@ -5,19 +5,21 @@ RSpec.describe Pricing::Prestige, type: :service do
   let!(:base_price) { Faker::Number.between(1, 99) }
   subject { described_class.new(base_price) }
 
-  it 'returns Float' do
-    VCR.use_cassette('yourlocalguardian') do
-      expect(subject.margin).to be_a(Float)
+  describe '#margin' do
+    it 'returns Fixnum' do
+      VCR.use_cassette('yourlocalguardian') do
+        expect(subject.margin).to be_a(Integer)
+      end
     end
-  end
 
-  it 'opens reuters url' do
-    VCR.use_cassette('yourlocalguardian') do
-      allow(subject).to receive(:open).
+    it 'opens reuters url' do
+      VCR.use_cassette('yourlocalguardian') do
+        allow(subject).to receive(:open).
           with('http://www.yourlocalguardian.co.uk/sport/rugby/rss/')
-      subject.margin
-      expect(subject).to have_received(:open).
+        subject.margin
+        expect(subject).to have_received(:open).
           with('http://www.yourlocalguardian.co.uk/sport/rugby/rss/')
+      end
     end
   end
 end
