6f96d475da6aa3764a509cfe8d6e425a2ecdc4ad
game <tawin.sup@gmail.com>
2017-02-17 00:06:30 +0700

    Implement expect response fro model type price endpoint

--- spec/requests/model_types_spec.rb
+++ spec/requests/model_types_spec.rb
@@ -41,4 +41,48 @@ RSpec.describe ModelTypesController, type: :request do
       end
     end
   end
+
+  describe '#show' do
+    context 'authentication' do
+      let!(:model) { create(:model_with_model_types) }
+      let!(:model_type) { model.model_types.first }
+
+      it 'should return authorization' do
+        VCR.use_cassette('prestige') do
+          post "/models/#{model.model_slug}/model_types_price/#{model_type.model_type_slug}",
+               nil,
+               Authorization: 'Token token="123"'
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      it 'should return unauthorization' do
+        VCR.use_cassette('prestige') do
+          post "/models/#{model.model_slug}/model_types_price/#{model_type.model_type_slug}",
+               nil,
+               Authorization: 'Token token="321"'
+          expect(response).to have_http_status(:unauthorized)
+        end
+      end
+
+      context 'return model_type' do
+        let!(:model) { create(:model_with_model_types) }
+
+        before do
+          VCR.use_cassette('prestige') do
+            post "/models/#{model.model_slug}/model_types_price/#{model_type.model_type_slug}",
+                 nil,
+                 Authorization: 'Token token="123"'
+          end
+        end
+
+        it 'return model_types for model' do
+          VCR.use_cassette('prestige') do
+            expect(response).to match_response_schema("model_type")
+          end
+        end
+      end
+
+    end
+  end
 end
