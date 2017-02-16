1921faaa71c4ef090548670c49f17660b76ad302
game <tawin.sup@gmail.com>
2017-02-16 21:26:31 +0700

    Implement prestige price

--- app/services/pricing/prestige.rb
+++ app/services/pricing/prestige.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 module Pricing
   class Prestige < Base
+    BASE_URL = 'http://www.yourlocalguardian.co.uk/sport/rugby/rss/'
+
+    def initialize(_base_price)
+      @base_price = _base_price
+    end
+
+    def margin
+      page = Nokogiri::XML(open(BASE_URL))
+      response = parse_body(page)
+      count_pub_date(response)
+    end
+
+    private def count_pub_date(_response)
+      -1
+    end
+
+    private def parse_body(response)
+      response.css('pubDate')
+    end
   end
 end
