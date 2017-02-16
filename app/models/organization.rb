8cb0183ecb911921991a92e14f8fad01bfa84e56
game <tawin.sup@gmail.com>
2017-02-16 19:51:16 +0700

    Implement enum and validate for organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,7 @@
 class Organization < ActiveRecord::Base
+  enum organization_type: [:show_room, :service, :dealer]
+  enum pricing_policy: [:flexible, :fixed, :prestige]
+
+  validates_presence_of :name, :public_name, :organization_type, :pricing_policy
+  validates_uniqueness_of :name, :public_name
 end
