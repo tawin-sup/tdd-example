282156adf3253b5b9a59fd7389ca9e2d6d3676a8
game <tawin.sup@gmail.com>
2017-02-16 19:56:03 +0700

    Implement validation for model_type

--- app/models/model_type.rb
+++ app/models/model_type.rb
@@ -1,2 +1,4 @@
 class ModelType < ActiveRecord::Base
+  validates_presence_of :name, :model_type_slug, :model_type_code, :base_price
+  validates_uniqueness_of :name, :model_type_code, :model_type_slug
 end
