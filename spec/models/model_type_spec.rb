69d6f296e5b0c0d203b4d09237035ef016a2481f
game <tawin.sup@gmail.com>
2017-02-16 19:55:30 +0700

    Add validation spec for model type

--- spec/models/model_type_spec.rb
+++ spec/models/model_type_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ModelType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validation
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:model_type_slug) }
+  it { is_expected.to validate_presence_of(:model_type_code) }
+  it { is_expected.to validate_presence_of(:base_price) }
+
+  it { is_expected.to validate_uniqueness_of(:name) }
+  it { is_expected.to validate_uniqueness_of(:model_type_slug) }
+  it { is_expected.to validate_uniqueness_of(:model_type_code) }
+
 end
