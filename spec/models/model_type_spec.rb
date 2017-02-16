6b9df592ce2e25e0916bdc19fb986ac3545eb2b8
game <tawin.sup@gmail.com>
2017-02-16 22:05:22 +0700

    Add total price correct service specs

--- spec/models/model_type_spec.rb
+++ spec/models/model_type_spec.rb
@@ -14,4 +14,56 @@ RSpec.describe ModelType, type: :model do
   it { is_expected.to validate_uniqueness_of(:name) }
   it { is_expected.to validate_uniqueness_of(:model_type_slug) }
   it { is_expected.to validate_uniqueness_of(:model_type_code) }
+
+  describe '#total_price' do
+    let!(:base_price) { Faker::Number.between(1, 99) }
+    let!(:total_price) { Faker::Number.between(1, 99) }
+    let!(:model_type) { create(:model_type) }
+    let!(:pricing_service) { double('pricing_service') }
+
+    context 'call Fixed price for Fixed organization' do
+      let!(:model) { create(:model_in_fixed) }
+
+      before do
+        model_type.model = model
+        allow(Pricing::Fixed).to receive(:new).and_return(pricing_service)
+        allow(pricing_service).to receive(:total_price).and_return(total_price)
+      end
+
+      it do
+        expect(Pricing::Fixed).to receive(:new).with(model_type.base_price)
+        model_type.total_price
+      end
+    end
+
+    context 'call Fixed price for Flexible organization' do
+      let!(:model) { create(:model_in_flexible) }
+
+      before do
+        model_type.model = model
+        allow(Pricing::Flexible).to receive(:new).and_return(pricing_service)
+        allow(pricing_service).to receive(:total_price).and_return(total_price)
+      end
+
+      it do
+        expect(Pricing::Flexible).to receive(:new).with(model_type.base_price)
+        model_type.total_price
+      end
+    end
+
+    context 'call Fixed price for Prestige organization' do
+      let!(:model) { create(:model_in_prestige) }
+
+      before do
+        model_type.model = model
+        allow(Pricing::Prestige).to receive(:new).and_return(pricing_service)
+        allow(pricing_service).to receive(:total_price).and_return(total_price)
+      end
+
+      it do
+        expect(Pricing::Prestige).to receive(:new).with(model_type.base_price)
+        model_type.total_price
+      end
+    end
+  end
 end
